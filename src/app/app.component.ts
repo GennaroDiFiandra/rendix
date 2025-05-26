c2e18d652af48f7ee42a39aa0f8d63ad7e132051
Gennaro Di Fiandra <gennarodifiandra@gmail.com>
2025-05-26 10:15:59 +0200

    (feat) Setup app UI with header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
+  styles: `:host {@apply min-h-screen flex flex-col [&>*:last-child]:mt-auto}`,
 })
 export class AppComponent {
 }
