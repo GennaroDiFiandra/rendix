c14ee65be66fee53fc248d48b17721d3c36cabdd
Gennaro Di Fiandra <gennarodifiandra@gmail.com>
2025-05-29 10:40:20 +0200

    (feat) Update app Ui with calculator and results components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
+import { Configuration } from './calculator/calculator.model';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { ResultsComponent } from './results/results.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CalculatorComponent, ResultsComponent],
   templateUrl: './app.component.html',
   styles: `:host {@apply min-h-screen flex flex-col [&>*:last-child]:mt-auto}`,
 })
 export class AppComponent {
+  configuration: Configuration<number> | null = null;
+
+  onGetConfiguration(configuration: Configuration<number>) {
+    this.configuration = configuration;
+  }
 }
